ee8153097bd4fb259f67cba1916f290f28f875c4
elyramirez <eramirez@redmagisterial.com>
2020-11-29 22:17:37 -0600

    add carousel to tools

--- src/app/component/home/tools/tools.component.ts
+++ src/app/component/home/tools/tools.component.ts
@@ -6,10 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tools.component.css']
 })
 export class ToolsComponent implements OnInit {
+  card: Array<any>=[];
+  constructor() { 
+    this.card = [{
+      imgpath: '../../../../assets/images/herramientas/Mesa de trabajo 50.png',
+      description: 'asdferff',
+      id: 0
+   
+     },
+     {
+       imgpath: '../../../../assets/images/herramientas/Mesa de trabajo 50.png',
+      description: 'asdferff',
+      id: 1
+     },
+     {
+       imgpath: '../../../../assets/images/herramientas/Mesa de trabajo 50.png',
+      description: 'asdferff',
+      id: 2
+     },
+     {
+       imgpath: '../../../../assets/images/herramientas/Mesa de trabajo 50.png',
+       description: 'asdferff',
+       id:3
+     },
+     {
+       imgpath: '../../../../assets/images/herramientas/Mesa de trabajo 50.png',
+      description: 'asdferff',
+      id:4
+     },
+     {
+       imgpath: '../../../../assets/images/herramientas/Mesa de trabajo 50.png',
+      description: 'asdferff',
+      id:5
+     },
+     {
+       imgpath: '../../../../assets/images/herramientas/Mesa de trabajo 50.png',
+       description: 'asdferff',
+       id:6
+     }
+   
+     ]
+     
+    
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
+
+  
+  
 
 }
